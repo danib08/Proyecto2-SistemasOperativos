c8f4587e2f4182e9668e233b6edb7ba78cf29337
Gabriel González <37197284+gabogh123@users.noreply.github.com>
2023-04-21 02:05:30 -0600

    Add(Definitions): Create functions

---
+++ Client/definitions.h
@@ -0,0 +1,5 @@
+#ifndef SERVER_CLIENT_OS2_IMAGE_SENDER_H
+#define SERVER_CLIENT_OS2_IMAGE_SENDER_H
+int get_file_extension(char* filename);
+int check_file_existence(char *filepath);
+#endif //SERVER_CLIENT_OS2_IMAGE_SENDER_H 
