df73912ac1cb1776dad9426d4f8c6746fcd20062
Gabriel González <37197284+gabogh123@users.noreply.github.com>
2023-04-26 20:05:07 -0600

    Start over

--- Client/definitions.h
+++
@@ -1,5 +0,0 @@
-#ifndef SERVER_CLIENT_OS2_IMAGE_SENDER_H
-#define SERVER_CLIENT_OS2_IMAGE_SENDER_H
-int get_file_extension(char* filename);
-int check_file_existence(char *filepath);
-#endif //SERVER_CLIENT_OS2_IMAGE_SENDER_H 
